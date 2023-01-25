5e932e93b6a3f1b587859e7b9f7bf245a7737bef
SirWesterfield <anastasicrl@gmail.com>
2023-01-25 09:57:41 -0600

    started project1

---
+++ Project1/location-proj1.cpp
@@ -0,0 +1,43 @@
+#include "location-proj1.h"
+#include <iostream>
+
+Location::Location()
+{
+    row = 0;
+    col = 0;
+    nextDirection = 0;
+}
+
+void Location::iterationBegin()
+{
+
+}
+
+Location Location::iterationCurrent() const
+{
+
+}
+void Location::iterationAdvance()
+{
+
+}
+
+bool Location::iterationDone() const
+{
+
+}
+
+bool Location::operator==(const Location &loc) const
+{
+
+}
+
+ostream &operator<<(ostream &os, const Location &loc)
+{
+
+}
+
+istream &operator>>(istream &is, Location &loc)
+{
+
+}

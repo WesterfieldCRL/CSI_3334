7e3ebe706220cc8ea2be7fe1fa7f21d910547dd7
SirWesterfield <anastasicrl@gmail.com>
2023-01-28 00:46:26 -0600

    fixed location class, created stack class

---
+++ Project1/stack-proj1.cpp
@@ -0,0 +1,43 @@
+#include "stack-proj1.h"
+#include <iostream>
+
+
+LocationStack::LocationStack()
+{
+
+}
+
+LocationStack::~LocationStack()
+{
+
+}
+
+void LocationStack::push(const Location &loc)
+{
+
+}
+
+void LocationStack::pop()
+{
+
+}
+
+const Location &LocationStack::getTop() const
+{
+
+}
+
+bool LocationStack::isEmpty(void) const
+{
+
+}
+
+bool LocationStack::isOn(const Location &loc) const
+{
+
+}
+
+ostream &operator<<(ostream &os, const LocationStack &s)
+{
+    
+}

eae373fdd1f49f92b9b1d26e7c9af4a9ce1b6db1
SirWesterfield <anastasicrl@gmail.com>
2023-01-30 09:56:04 -0600

    started driver

--- Project1/driver-proj1.cpp
+++ Project1/driver-proj1.cpp
@@ -7,154 +7,50 @@ using namespace std;
 
 int main()
 {
-    Location loc1;
-    LocationStack locStack1;
     Maze maze;
+    Location m;
+    Location neighbor;
+    LocationStack stack;
+    bool NoSolution = false;
 
-    cout << "testing with NULL top" << endl;
-
-    cout << "printing stack" << endl;
-    cout << locStack1;
-
-    cout << "testing isOn with empty Stack" << endl;
-    if (locStack1.isOn(loc1))
-    {
-        cout << "bad, isOn is true" << endl;
-    }
-    else
-    {
-        cout << "good, isOn is false" << endl;
-    }
-
-    cout << "testing isEmpty" << endl;
-    if (locStack1.isEmpty())
-    {
-        cout << "good, isEmpty is true" << endl;
-    }
-    else
-    {
-        cout << "bad, isEmpty is false" << endl;
-    }
-
-    cout << endl;
-
-    cout << "adding 5 locations to the stack" << endl;
-
-    for (int i = 0; i < 5; i++)
-    {
-        cout << "input a location" << endl;
-        cin >> loc1;
-        locStack1.push(loc1);
-        cout << locStack1.getTop() << endl;
-    }
-
-    cout << "printing stack" << endl;
-    cout << locStack1;
-
-    cout << "testing isOn" << endl;
-    if (locStack1.isOn(loc1))
-    {
-        cout << "good, isOn is true" << endl;
-    }
-    else
-    {
-        cout << "bad, isOn is false" << endl;
-    }
-
-    cout << "testing isEmpty" << endl;
-    if (locStack1.isEmpty())
-    {
-        cout << "bad, isEmpty is true" << endl;
-    }
-    else
-    {
-        cout << "good, isEmpty is false" << endl;
-    }
-
-    cout << "printing stack again" << endl;
-    cout << locStack1;
-
-    cout << endl;
-
-    cout << "testing pop" << endl;
-
-    for (int i = 0; i < 5; i++)
-    {
-        cout << locStack1.getTop() << endl;
-        locStack1.pop();
-    }
-
-    cout << "printing stack" << endl;
-    cout << locStack1;
-
-    cout << "testing isOn with empty Stack" << endl;
-    if (locStack1.isOn(loc1))
-    {
-        cout << "bad, isOn is true" << endl;
-    }
-    else
-    {
-        cout << "good, isOn is false" << endl;
-    }
-
-    cout << "testing isEmpty" << endl;
-    if (locStack1.isEmpty())
-    {
-        cout << "good, isEmpty is true" << endl;
-    }
-    else
-    {
-        cout << "bad, isEmpty is false" << endl;
-    }
-
-    cout << endl << "testing maze" << endl;
-
-    cout << "input maze" << endl;
     cin >> maze;
 
-    cout << "start location = " << maze.getStartLocation() << endl;
+    m = maze.getStartLocation();
+    stack.push(m);
 
-    for (int i = 0; i < 2; i++)
+    while (!NoSolution && !maze.isEndLocation(stack.getTop()))
     {
-        Location temp;
-        cout << "input location" << endl;
-        cin >> temp;
-        if (maze.isValidLocation(temp))
+        m = stack.getTop();
+        stack.pop();
+        if (!m.iterationDone())
         {
-            cout << "is valid" << endl;
+            neighbor = m.iterationCurrent();
+            neighbor.iterationBegin();
+            m.iterationAdvance();
+            stack.push(m);
+            if (!stack.isOn(neighbor) && maze.isValidLocation(neighbor))
+            {
+                stack.push(neighbor);
+            }
+            //cout << stack << endl << endl;
+            //cout << "n = " << neighbor << endl;
         }
-        else
+        else if (stack.isEmpty())
         {
-            cout << "not valid" << endl;
+            NoSolution = true;
         }
     }
 
-    cout << "input end location" << endl;
-
-    cin >> loc1;
-    if (maze.isEndLocation(loc1))
+    if (!NoSolution)
     {
-        cout << "good" << endl;
+        cout << "Solution found" << endl;
+        cout << stack;
     }
     else
     {
-        cout << "bad" << endl;
+        cout << "No solution" << endl;
     }
 
-    cout << "input non-end location" << endl;
-
-    cin >> loc1;
-
-    if (maze.isEndLocation(loc1))
-    {
-        cout << "bad" << endl;
-    }
-    else
-    {
-        cout << "good" << endl;
-    }
-
-    cout << "Done" << endl;
 
     return 0;
 }

90d6e915dc33a0c1284cd810e9753deacbd0627b
SirWesterfield <anastasicrl@gmail.com>
2023-02-06 09:46:43 -0600

    finished extended fib

--- Homework1/extendedFib.cpp
+++ Homework1/extendedFib.cpp
@@ -4,11 +4,34 @@ using namespace std;
 
 int extendedFib(int n)
 {
-    
+    if (n > 0 && n < 4) 
+    {
+        return 1;
+    } 
+    else if (n > 3) 
+    {
+        return extendedFib(n-1)+extendedFib(n-2)+extendedFib(n-3);
+    } 
+    else 
+    {
+        return extendedFib(n+3)-(extendedFib(n+1)+extendedFib(n+2));
+    }
 }
 
 int main()
 {
-
+    int n;
+    cin >> n;
+    cout << extendedFib(n) << endl;
     return 0;
 }
+
+/*
+Inputs:
+4: 3
+0: -1
+-3: -3
+9: 57
+20: 46499
+-20: -477
+*/

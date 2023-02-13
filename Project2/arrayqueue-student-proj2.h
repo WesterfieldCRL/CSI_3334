8853aa30a0caca1c2e146e6db4d5e66ff9ef5240
Wesley Anastasi <anastasicrl@gmail.com>
2023-02-13 00:09:57 -0600

    started arrayQueue

--- Project2/arrayqueue-student-proj2.h
+++ Project2/arrayqueue-student-proj2.h
@@ -4,10 +4,63 @@
 #include "arrayqueue-prof-proj2.h"
 #include <cassert>
 
-/********************************************************* 
- * PUT YOUR CODE HERE                                    * 
- *********************************************************/
+template <class Base>
+ArrayQueue<Base>::ArrayQueue()
+{
+    this->capacity = 1;
+    this->length = 1;
+    this->front = 0;
+    this->data = new Base[this->capacity];
+}
 
+template <class Base>
+ArrayQueue<Base>::ArrayQueue(const ArrayQueue<Base> &q)
+{
+    this->data = new Base[q.capacity];
+    this->length = q.length;
+    this->capacity = q.capacity;
+
+    for (int i = 0; i < q.capacity; i++)
+    {
+        this->data[i] = q.data[i];
+    }
+}
+
+template <class Base>
+ArrayQueue<Base>::~ArrayQueue()
+{
+    delete [] this->data;
+}
+
+template <class Base>
+void ArrayQueue<Base>::add(const Base &item)
+{
+
+}
+
+template <class Base>
+void ArrayQueue<Base>::remove()
+{
+
+}
+
+template <class Base>
+const Base &ArrayQueue<Base>::getFront() const
+{
+    return this->data[this->front];
+}
+
+template <class Base>
+int ArrayQueue<Base>::getLength() const
+{
+    return this->length;
+}
+
+template <class Base>
+const ArrayQueue<Base> &ArrayQueue<Base>::operator=(const ArrayQueue<Base> &q)
+{
+
+}
 
 
 #endif

81a61348fec98257480f0ad0a559aded7cad2101
Wesley Anastasi <anastasicrl@gmail.com>
2023-02-21 21:02:48 -0600

    started Project3

---
+++ Project3/bst-student-proj3.h
@@ -0,0 +1,45 @@
+#ifndef BST_STUDENT_PROJ3 
+#define BST_STUDENT_PROJ3 
+
+#include "bst-prof-proj3.h"
+
+/* Place your implementation of the BSTNode, BST, and EncryptionTree classes
+ * here.
+ *
+ * Your driver should #include this file, which (as you can see above) will also
+ * #include the professor file.
+ */
+
+template <class Base>
+BSTNode<Base>::~BSTNode()
+{
+
+}
+
+template <class Base>
+void BSTNode<Base>::printPreorder(ostream &os = cout, string indent = "") const
+{
+
+}
+
+template <class Base>
+void BSTNode<Base>::verifySearchOrder() const
+{
+
+}
+
+template <class Base>
+const BSTNode BSTNode<Base>::*minNode() const
+{
+
+}
+
+template <class Base>
+const BSTNode<Base>BSTNode *maxNode() const
+{
+
+}
+
+
+#endif
+

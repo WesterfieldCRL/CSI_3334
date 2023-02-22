50f6cac1ad239da1c90fcd44d9ea2d288f18a787
SirWesterfield <anastasicrl@gmail.com>
2023-02-22 12:02:41 -0600

    finished BSTNode, started BST

--- Project3/bst-student-proj3.h
+++ Project3/bst-student-proj3.h
@@ -13,39 +13,153 @@
 template <class Base>
 BSTNode<Base>::~BSTNode()
 {
-
+    if (this->left != NULL)
+    {
+        delete this->left;
+    }
+    if (this->right != NULL)
+    {
+        delete this->right;
+    }
 }
 
 template <class Base>
 void BSTNode<Base>::printPreorder(ostream &os, string indent) const
 {
-
+    os << indent << this->data << endl;
+    if (this->left != NULL)
+    {
+        this->left->printPreorder(os, indent + "  ");
+    }
+    if (this->right != NULL)
+    {
+        this->right->printPreorder(os, indent + "  ");
+    }
 }
 
 template <class Base>
 const BSTNode<Base> *BSTNode<Base>::minNode() const
 {
-
+    BSTNode *temp = this;
+    while (temp->left != NULL)
+    {
+        temp = temp->left;
+    }
+    return temp;
 }
 
 template <class Base>
 const BSTNode<Base> *BSTNode<Base>::maxNode() const
 {
-
+    BSTNode *temp = this;
+    while (temp->right != NULL)
+    {
+        temp = temp->right;
+    }
+    return temp;
 }
 
 template <class Base>
-BSTNode<Base> BSTNode<Base>::(const BSTNode &t)
+void BST<Base>::insert(const Base &item)
 {
 
+    if (this->root == NULL)
+    {
+        this->root = new BSTNode<Base>(item);
+    }
+    else
+    {
+        BSTNode<Base> temp = this->root;
+        while (temp != NULL)
+        {
+            if (item < temp->data)
+            {
+                if (temp->left != NULL)
+                {
+                    temp = temp->left;
+                }
+                else
+                {
+                    temp->left = new BSTNode<Base>(item);
+                    temp = NULL;
+                }
+            }
+            else
+            {
+                if (temp->right != NULL)
+                {
+                    temp = temp->right;
+                }
+                else
+                {
+                    temp->right = new BSTNode<Base>(item);
+                    temp = NULL;
+                }
+            }
+        }
+        delete temp;
+    }
 }
 
 template <class Base>
-const BSTNode<Base> &BSTNode<Base>::operator=(const BSTNode &n) 
+void BST<Base>::remove(const Base &item)
 {
+    //find the item
+    BSTNode<Base> *toRemove = this->root;
+    BSTNode<Base> *parent = NULL;
+    while (toRemove != NULL || toRemove->data != item)
+    {
+        parent = toRemove;
+        if (item < toRemove->data)
+        {
+            toRemove = toRemove->left;
+        }
+        else
+        {
+            toRemove = toRemove->right;
+        }
+    }
+    
+    if (toRemove != NULL)
+    {
+        //if the item is a leaf
+        if (toRemove->left == NULL && toRemove->right == NULL)
+        {
+            if (parent == NULL)
+            {
+                this->root = NULL;
+            }
+            else
+            {
+                if (parent->left == toRemove)
+                {
+                    parent->left = NULL;
+                }
+                else
+                {
+                    parent->right = NULL;
+                }
+            }
+        }
+        //if the item has one child
+        else if (toRemove->left == NULL || toRemove->right == NULL)
+        {
 
+        }
+        //if the item has two children
+        else
+        {
+
+        }
+
+        //delete pointers
+        if (parent != NULL)
+        {
+            delete parent;
+        }
+        delete toRemove;
+    }
 }
-
 
 #endif
 

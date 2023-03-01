d180808034e8c2b64e79820560c47ad006bf4648
SirWesterfield <anastasicrl@gmail.com>
2023-03-01 11:59:32 -0600

    documentation for BSTNode, BST, and EncryptionTree

--- Project3/bst-student-proj3.h
+++ Project3/bst-student-proj3.h
@@ -4,9 +4,11 @@
  * Student name: Wesley Anastasi
  * version: 1.0
  * 
- * This file contains all the functions for the Maze class.
- * The functions include the default constructor, the getStartLocation function,
- * the isValidLocation function, the isEndLocation function, and the operator>> function.
+ * This file contains all the functions for the BSTNode, BST and EncryptionTree classes.
+ * The functions implemented in BSTNode are the destructor, printPreorder, minNode, and maxNode.
+ * The functions implemented in BST are the insert and remove functions.
+ * The functions implemented in EncryptionTree are the encrypt and decrypt functions.
+ * BST uses BSTNode for its functions and EncryptionTree inherits from BST.
  * 
  */
 
@@ -16,6 +18,17 @@
 #include "bst-prof-proj3.h"
 
 //BSTNode
+
+/**
+ * ~BSTNode
+ * 
+ * Destructor for BSTNode. Deletes the left and right nodes recursively.
+ * 
+ * Parameters:
+ *   none
+ * 
+ * Return value: none
+ */
 
 template <class Base>
 BSTNode<Base>::~BSTNode()
@@ -29,6 +42,19 @@ BSTNode<Base>::~BSTNode()
         delete this->right;
     }
 }
+
+/**
+ * printPreorder
+ * 
+ * Prints the tree in preorder. Prints the data of the node, then recursively
+ * calls printPreorder on the left node and then the right node with an indent added.
+ * 
+ * Parameters:
+ *   os - output stream to print to
+ *   indent - string to indent the tree
+ * 
+ * Return value: none
+ */
 
 template <class Base>
 void BSTNode<Base>::printPreorder(ostream &os, string indent) const
@@ -52,6 +78,19 @@ void BSTNode<Base>::printPreorder(ostream &os, string indent) const
     }
 }
 
+/**
+ * minNode
+ * 
+ * Finds the minimum node in the tree. If the left node is NULL, the current node
+ * is the minimum node. If the left node is not NULL, recursively calls minNode
+ * on the left node.
+ * 
+ * Parameters:
+ *   none
+ * 
+ * Return value: pointer to the minimum node
+ */
+
 template <class Base>
 const BSTNode<Base> *BSTNode<Base>::minNode() const
 {
@@ -64,6 +103,19 @@ const BSTNode<Base> *BSTNode<Base>::minNode() const
         return this->left->minNode();
     }
 }
+
+/**
+ * maxNode
+ * 
+ * Finds the maximum node in the tree. If the right node is NULL, the current node
+ * is the maximum node. If the right node is not NULL, recursively calls maxNode
+ * on the right node.
+ * 
+ * Parameters:
+ *   none
+ * 
+ * Return value: pointer to the maximum node
+ */
 
 template <class Base>
 const BSTNode<Base> *BSTNode<Base>::maxNode() const
@@ -79,6 +131,22 @@ const BSTNode<Base> *BSTNode<Base>::maxNode() const
 }
 
 //BST
+
+/**
+ * insert
+ * 
+ * Inserts an item into the tree. If the root is NULL, the item is inserted
+ * as the root. If the root is not NULL, a temporary node is created and
+ * traverses the tree until it finds a NULL node. The item is then inserted
+ * as the left or right node of the temporary node depending on if it is
+ * less than or greater than the temporary node.
+ * 
+ * 
+ * Parameters:
+ *   item - item to be inserted
+ * 
+ * Return value: none
+ */
 
 template <class Base>
 void BST<Base>::insert(const Base &item)
@@ -111,6 +179,26 @@ void BST<Base>::insert(const Base &item)
         }
     }
 }
+
+/**
+ * remove
+ * 
+ * Removes an item from the tree. First, the item is found in the tree.
+ * and then then the item is removed based on the following cases:
+ * 1. The item is a leaf
+ *    If the parent is NULL, the root is set to NULL.
+ *    If the parent is not NULL, then whichever child of the parent is the item
+ *    is set to NULL.
+ * 2. The item has one child
+ *    If the node to be removed is set to its child.
+ * 3. The item has two children
+ *    The minimum node of the right subtree is found and set to the node to be removed.
+ * 
+ * Parameters:
+ *   item - item to be removed
+ * 
+ * Return value: none
+ */
 
 template <class Base>
 void BST<Base>::remove(const Base &item)
@@ -147,11 +235,11 @@ void BST<Base>::remove(const Base &item)
         else if (toRemove->left == NULL || toRemove->right == NULL) {
             BSTNode<Base> *child = NULL;
             if (toRemove->left != NULL) {
-                child = toRemove;
+                child = toRemove->left;
                 toRemove->left = NULL;
             }
             else {
-                child = toRemove;
+                child = toRemove->right;
                 toRemove->right = NULL;
             }
             if (parent->left == toRemove) {
@@ -160,6 +248,7 @@ void BST<Base>::remove(const Base &item)
             else {
                 parent->right = child;
             }
+            child = NULL;
             delete child;
         }
         //if the item has two children
@@ -170,9 +259,17 @@ void BST<Base>::remove(const Base &item)
                 leftMostParent = leftMost;
                 leftMost = leftMost->left;
             }
-            leftMostParent->left = leftMost->right;
-            leftMost->right = toRemove->right;
-            leftMost->left = toRemove->left;
+            //if the leftmost node is the right child of the node to be removed
+            if (leftMost == toRemove->right) { 
+                leftMost->left = toRemove->left;
+            }
+            //if the leftmost node is not the right child of the node to be removed
+            else
+            {
+                leftMostParent->left = leftMost->right;
+                leftMost->right = toRemove->right;
+                leftMost->left = toRemove->left;
+            }
             if (parent->left == toRemove) {
                 parent->left = leftMost;
             }
@@ -180,17 +277,34 @@ void BST<Base>::remove(const Base &item)
                 parent->right = leftMost;
             }
             toRemove->left = toRemove->right = NULL;
-            delete toRemove;
+            leftMost = NULL;
+            leftMostParent = NULL;
+            delete leftMost;
+            delete leftMostParent;
         }
 
         //delete pointers
-        /*if (parent != NULL)
-        {
-            delete parent;
-        }
-        delete toRemove;*/
+
+        toRemove = NULL;
+        parent = NULL;
+
+        delete parent;
+        delete toRemove;
     }
 }
+
+/**
+ * encrypt
+ * 
+ * Encrypts an item by finding the item in the tree and returning the path
+ * to the item. If the item is not found, the path is set to "?".
+ * 
+ * Parameters:
+ *   item - item to be found
+ * 
+ * Return value: the path to the item in the form r01 (r is the root, 0 is left, 1 is right)
+ */
+
 
 template <class Base>
 string EncryptionTree<Base>::encrypt(const Base &item) const {
@@ -220,6 +334,18 @@ string EncryptionTree<Base>::encrypt(const Base &item) const {
 
     return result;
 }
+
+/**
+ * decrypt
+ * 
+ * Decrypts an item by following the provided path.
+ * If the path is invalid, NULL is returned.
+ * 
+ * Parameters:
+ *   path - path to the item in the form r01 (r is the root, 0 is left, 1 is right)
+ * 
+ * Return value: pointer to the item
+ */
 
 template <class Base>
 const Base *EncryptionTree<Base>::decrypt(const string &path) const {

1b50bb3117760051cac6879e3f7882d141343bb5
WesterfieldCRL <anastasicrl@gmail.com>
2023-04-21 08:59:07 -0500

    In Class Activity 13

--- InCalssActivity13/main.cpp
+++ InCalssActivity13/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <list>
+#include <algorithm>
 
 using namespace std;
 
@@ -55,12 +56,46 @@ class AdjacencyMatrixGraph {
 
 class AdjacencyListGraph {
     public:
-        AdjacencyListGraph(int num_vertices); // initialize adj_list
-        void add_edge(int u, int v); // add a directed edge from u to v
-        bool edge_exists(int u, int v) const; // return true if there is an edge from u to v
-        void print_edges(int u) const; // from vertex u
-        void print_edges() const; // from all vertices
-        int count_all_edges() const; // in the entire graph
+        AdjacencyListGraph(int num_vertices) : adj_list(num_vertices) {}
+        void add_edge(int u, int v)
+        {
+            adj_list[u].push_back(v);
+        }
+        bool edge_exists(int u, int v) const
+        {
+            return find(adj_list[u].begin(), adj_list[u].end(), v) != adj_list[u].end();
+            /*for (list<int>::const_iterator it = adj_list[u].begin(); it != adj_list[u].end(); it++)
+            {
+                if (*it == v)
+                {
+                    return true;
+                }
+            }
+            return false;*/
+        }
+        void print_edges(int u) const
+        {
+            for (list<int>::const_iterator it = adj_list[u].begin(); it != adj_list[u].end(); it++)
+            {
+                cout << u << " -> " << *it << endl;
+            }
+        }
+        void print_edges() const
+        {
+            for (int i = 0; i < adj_list.size(); i++)
+            {
+                print_edges(i);
+            }
+        }
+        int count_all_edges() const
+        {
+            int count = 0;
+            for (int i = 0; i < adj_list.size(); i++)
+            {
+                count += adj_list[i].size();
+            }
+            return count;
+        }
     private:
         vector<list<int> > adj_list; // a vector of V lists
         // adj_list[u] is a list with the destinations of all edges leaving vertex u

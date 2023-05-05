3b97cebf538dc7bef7a0792fa88451555b4a3a2a
WesterfieldCRL <anastasicrl@gmail.com>
2023-05-05 13:56:14 -0500

    further testing

--- Project6/genAndTest.sh
+++ Project6/genAndTest.sh
@@ -3,6 +3,6 @@ cmp studentOut.txt profOutput6.txt
 until [ $? -ne 0 ]
 do
 ./inputGen
-./a.out < input.txt > studentOutput.txt
-./project6_solution_linux_intel < input.txt > profSolution.txt
+./a.out < input.txt > studentOut.txt
+./project6_solution_linux_intel < input.txt > profOutput6.txt
 done

2d144a8f99afd1cf5a5f1a6d28baf8bdf811961a
Wesley Anastasi <anastasicrl@gmail.com>
2023-05-08 02:10:53 -0500

    simplified driver

--- Project6/driver-proj6.cpp
+++ Project6/driver-proj6.cpp
@@ -21,7 +21,6 @@
 #include "arrayheap-student-proj6.h"
 #include "graph-proj6.h"
 
-
 using namespace std;
 
 /**
@@ -40,174 +39,107 @@ using namespace std;
 
 enum { INFINITE_COST = 1000000000 };
 
-int main() {
-    ArrayHeap<pair<int, int>> serverCosts; //cost, vertex
-    map<string, int> computerToIndex;
-    //vector<bool> canBeServer;
-    //vector<string> computers1;
-    //vector<string> computers2;
-    //vector<int> costs;
-    
-    string computer1;
-    string computer2;
-    int cost;
-    string temp;
-    int totalCost;
-    //int tempDistances;
-    bool isTooBig;
-    
-
-    int edges;
+int main () {
+    int edges = 0;
     int vertices = 0;
+    vector<bool> canBeServer;
+    queue<int> edgeQueue;
+    map<int, string> computerToIndex;
+
+
+    //read in data
     cin >> edges;
-
-    int costs[edges];
-    bool servers[edges];
-    pair<int, int> edgeCombos[edges];
-    //Graph graph(edges);
-    
     for (int i = 0; i < edges; i++) {
+        string tempInput;
         
-        cin >> computer1 >> computer2 >> cost; //read in data
-
-        //Update map of computer names to index
-        if (computerToIndex.size() == 0) {
-            //if map is empty, add both computers to map
-            computerToIndex[computer1] = 0;
-            computerToIndex[computer2] = 1;
-            vertices = 2;
-            //check if a server
-            servers[0] = false;
-            if (computer1.length() > 7) {
-                temp = computer1.substr(computer1.length()-7, 7);
-                if (temp == "_server") {
-                    servers[0] = true;
+        for (int j = 0; j < 2; j++) {
+            cin >> tempInput;
+            //add to map if not already in map
+            bool isInMap = false;
+            for (auto it = computerToIndex.begin(); it != computerToIndex.end(); it++) {
+                if (it->second == tempInput) {
+                    isInMap = true;
                 }
             }
-            servers[1] = false;
-            if (computer2.length() > 7) {
-                temp = computer2.substr(computer2.length()-7, 7);
-                if (temp == "_server") {
-                    servers[1] = true;
+
+            if (!isInMap) {
+                computerToIndex.insert(make_pair(vertices, tempInput));
+                vertices++;
+
+                if (tempInput.size() > 7 && tempInput.substr(tempInput.length()-7, 7) == "_server") {
+                    canBeServer.push_back(true);
+                }
+                else {
+                    canBeServer.push_back(false);
+                }
+            }
+
+
+            for (auto it = computerToIndex.begin(); it != computerToIndex.end(); it++) {
+                if (it->second == tempInput) {
+                    edgeQueue.push(it->first);
                 }
             }
         }
-        else {
-            //check if computer already in map
-            if (computerToIndex.find(computer1) == computerToIndex.end()) {
-                computerToIndex[computer1] = vertices;
-                
-                //check if a server
-                servers[vertices] = false;
-                if (computer1.length() > 7) {
-                    temp = computer1.substr(computer1.length()-7, 7);
-                    if (temp == "_server") {
-                        servers[vertices] = true;
-                    }
-                }
-                vertices++;
-            }
-            if (computerToIndex.find(computer2) == computerToIndex.end()) {
-                computerToIndex[computer2] = vertices;
-                
-                //check if a server
-                servers[vertices] = false;
-                if (computer2.length() > 7) {
-                    temp = computer2.substr(computer2.length()-7, 7);
-                    if (temp == "_server") {
-                        servers[vertices] = true;
-                    }
-                }
-                vertices++;
-            }
-        }
 
-        edgeCombos[i] = make_pair(computerToIndex[computer1], computerToIndex[computer2]);
-        costs[i] = cost;
+        int tempCost;
+        cin >> tempCost;
+        edgeQueue.push(tempCost);
     }
 
+    //create graph
     Graph graph(vertices);
-    //int tempCosts = costs.size();
-    for (int i = 0; i < edges; i++) {
-        graph.addEdge(edgeCombos[i].first, edgeCombos[i].second, costs[i]);
+
+    //add edges to graph
+    while (!edgeQueue.empty()) {
+        int tempComp1 = edgeQueue.front();
+        edgeQueue.pop();
+        int tempComp2 = edgeQueue.front();
+        edgeQueue.pop();
+        int tempCost = edgeQueue.front();
+        edgeQueue.pop();
+
+        graph.addEdge(tempComp1, tempComp2, tempCost);
     }
 
-    //int tempServerSize = canBeServer.size();
+    ArrayHeap<pair<int, string>> outputHeap; //cost, vertex
+
+    //call dijkstra on servers
     for (int i = 0; i < vertices; i++) {
-        if (servers[i]) {
+        if (canBeServer[i]) {
             vector<int> distances;
             distances = graph.dijkstra(i);
-            totalCost = 0;
-            //tempDistances = distances.size();
-            isTooBig = false;
+            int totalCost = 0;
+            bool isTooBig = false;
             for (int j = 0; j < vertices; j++) {
                 if (distances.back() >= INFINITE_COST) {
                     isTooBig = true;
                     j = vertices;
                 }
-                else
-                {
+                else {
                     totalCost += distances.back();
                     distances.pop_back();
                 }
             }
             if (!isTooBig) {
-                serverCosts.insert(make_pair(totalCost, i));
+                outputHeap.insert(make_pair(totalCost, computerToIndex[i]));
             }
         }
     }
 
-    pair<int, int> min = serverCosts.getMinItem();
-
-    if (serverCosts.getNumItems() == 0) {
+    //output
+    if (outputHeap.getNumItems() == 0) {
         cout << "no server can serve the whole network" << endl;
     }
     else {
-        //vector<string> outputAlphabetically;
-        string outputAlphabetically[vertices];
-        int numOutputs = 0;
-        cout << "total delay: " << min.first << endl;
-        serverCosts.removeMinItem();
+        int minDelay = outputHeap.getMinItem().first;
+        cout << "total delay: " << minDelay << endl;
 
-        for (auto it = computerToIndex.begin(); it != computerToIndex.end(); it++) {
-            if (it->second == min.second) {
-                outputAlphabetically[numOutputs] = it->first;
-                numOutputs++;
-            }
+        while (outputHeap.getNumItems() > 0 && outputHeap.getMinItem().first == minDelay) {
+            cout << outputHeap.getMinItem().second << endl;
+            outputHeap.removeMinItem();
         }
-
-        while (serverCosts.getMinItem().first == min.first && serverCosts.getNumItems() > 0) {
-            for (auto it = computerToIndex.begin(); it != computerToIndex.end(); it++) {
-                if (it->second == serverCosts.getMinItem().second) {
-                    outputAlphabetically[numOutputs] = it->first;
-                    numOutputs++;
-                }
-            }
-            serverCosts.removeMinItem();
-        }
-
-        //int tempValue = outputAlphabetically.size();
-
-        bool swap = false;
-        do {
-            swap = false;
-            for (int i = 0; i < numOutputs-1; i++) {
-                if (outputAlphabetically[i] > outputAlphabetically[i+1]) {
-                    string tempOutput;
-                    tempOutput = outputAlphabetically[i];
-                    outputAlphabetically[i] = outputAlphabetically[i+1];
-                    outputAlphabetically[i+1] = tempOutput;
-                    swap = true;
-                }
-            }
-        } while(swap);
-
-        for (int i = 0; i < numOutputs; i++) {
-            cout << outputAlphabetically[i] << endl;
-        }
-
     }
-
 
     return 0;
 }
